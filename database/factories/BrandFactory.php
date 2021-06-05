947c2cace39959a802582177b83876700e9926d0
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-06-05 06:05:15 +0200

    Add brand model and migration

--- database/factories/BrandFactory.php
+++ database/factories/BrandFactory.php
@@ -2,10 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Models\Category;
+use App\Models\Brand;
 use Faker\Generator as Faker;
 
-$factory->define(Category::class, function (Faker $faker) {
+$factory->define(Brand::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence()
     ];
