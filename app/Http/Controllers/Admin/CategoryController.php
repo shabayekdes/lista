8d8d6865024d8f72113804a9610a6ee8b0e86c00
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-06-05 03:11:52 +0200

    Add category admin crud controller

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\CategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+
+        return $this->jsonResponse('List categories', $categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  CategoryRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create($request->all());
+
+        return $this->jsonResponse('Category added successfully', $category, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return $this->jsonResponse('Category added successfully', $category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $category->update($request->all());
+        return $this->jsonResponse('Category updated successfully', $category, 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        try {
+            $category->delete();
+            return $this->jsonResponse('Category deleted successfully');
+        } catch (\Exception $e) {
+            return $this->errorResponse("No query results for category: $id", "Invalid data");
+        }
+    }
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,41 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Json response successfully
+     *
+     * @param string $message
+     * @param [type] $data
+     * @param integer $code
+     * @return void
+     */
+    public function jsonResponse($message = "", $data = null, $code = 200)
+    {
+    	return response()->json([
+    		"code" => $code,
+    		"message" => $message,
+    		"data" => $data
+    	], 200);
+    }
+    /**
+     * Erorr response
+     *
+     * @param [type] $userMessage
+     * @param [type] $internalMessage
+     * @param string $moreInfo
+     * @param integer $code
+     * @return void
+     */
+    public function errorResponse($userMessage, $internalMessage, $moreInfo = "", $code = 400)
+    {
+    	return response()->json([
+    		"code" => $code,
+    		"message" => $userMessage,
+    		"errors" => [
+    			"errorMessage" => $internalMessage,
+    			"errorDetails" => $moreInfo,
+    		]
+    	], 200);
+    }
 }
