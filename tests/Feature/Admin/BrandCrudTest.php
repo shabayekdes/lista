388968f09d2c1d19e90476b16f8ca129e5dfbbda
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-06-11 01:53:39 +0200

    Change name required validation test on category and brand

--- tests/Feature/Admin/BrandCrudTest.php
+++ tests/Feature/Admin/BrandCrudTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Admin;
 use Tests\TestCase;
 
 use App\Models\Brand;
+use Illuminate\Support\Arr;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BrandCrudTest extends TestCase
@@ -23,28 +24,34 @@ class BrandCrudTest extends TestCase
         $response = $this->get('/api/admin/brands')
                 ->assertStatus(200);
     }
-      /**
+    /**
      * A basic feature test example.
      *
      * @return void
      */
     public function test_can_store_brand_name_is_required()
     {
-        $this->withoutExceptionHandling();
+        $this->withExceptionHandling();
 
-        $data = [
-            'name' => $this->faker->sentence()
-        ];
+        $data = $this->getData();
 
-        $response = $this->post('/api/admin/brands', $data);
+        Arr::forget($data, 'name');
+
+        $response = $this->withHeaders([
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json'
+            ])->post('/api/admin/brands', $data);
+
 
         $response->assertStatus(200);
 
         $result = $response->json();
 
-        $this->assertEquals(201, $result['code']);
-        $this->assertEquals(1, Brand::count());
-        $this->assertEquals($data['name'], $result['data']['name']);
+        $this->assertEquals(422, $result['code']);
+        $this->assertEquals("The name field is required.", $result['message']);
+
+        $this->assertTrue(Arr::has($result, 'errors.errorDetails.name'));
+
     }
      /**
      * A basic feature test example.
@@ -111,5 +118,12 @@ class BrandCrudTest extends TestCase
 
         $this->assertEquals(1, Brand::count());
         $this->assertEquals($brand->name, $result['data']['name']);
+    }
+
+    private function getData()
+    {
+        return [
+            'name' => $this->faker->sentence()
+        ];
     }
 }
--- tests/Feature/Admin/CategoryCrudTest.php
+++ tests/Feature/Admin/CategoryCrudTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Admin;
 
 use Tests\TestCase;
 use App\Models\Category;
+use Illuminate\Support\Arr;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -43,21 +44,26 @@ class CategoryCrudTest extends TestCase
      */
     public function test_can_store_category_name_is_required()
     {
-        // $this->withoutExceptionHandling();
+        $this->withExceptionHandling();
 
-        $data = [
-            'name' => $this->faker->sentence()
-        ];
+        $data = $this->getData();
 
-        $response = $this->post('/api/admin/categories', $data);
+        Arr::forget($data, 'name');
+
+        $response = $this->withHeaders([
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json'
+            ])->post('/api/admin/categories', $data);
+
 
         $response->assertStatus(200);
 
         $result = $response->json();
 
-        $this->assertEquals(201, $result['code']);
-        $this->assertEquals(1, Category::count());
-        $this->assertEquals($data['name'], $result['data']['name']);
+        $this->assertEquals(422, $result['code']);
+        $this->assertEquals("The name field is required.", $result['message']);
+
+        $this->assertTrue(Arr::has($result, 'errors.errorDetails.name'));
     }
     /**
      * A basic feature test example.
@@ -144,5 +150,12 @@ class CategoryCrudTest extends TestCase
 
         $this->assertEquals(1, Category::count());
         $this->assertEquals($category->name, $result['data']['name']);
+    }
+
+    private function getData()
+    {
+        return [
+            'name' => $this->faker->sentence()
+        ];
     }
 }
